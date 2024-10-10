b64657b42122d04d4f1ff47a5ea1c20715616117
SURYA K <131654704+surya03-it@users.noreply.github.com>
2024-10-10 11:44:19 +0530

    Update mysqlConnect.php

--- mysqlConnect.php
+++ mysqlConnect.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysqli_connect("localhost", "root", "password", "smart_users") OR die('Network connection error. Check connection then reload');
+$con=mysqli_connect("localhost", "root", "new_password", "smart_users") OR die('Network connection error. Check connection then reload');
 /*
 $con=mysqli_connect("localhost","root","pass123","smart_users") OR die('Network connection error. Check connection then reload');
 */
